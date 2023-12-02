3d00707e2e97f71c754bca484dd3aecb388355cc
Soulrippper8811 <134708634+Soulripper8811@users.noreply.github.com>
2023-12-02 16:11:22 +0530

    youtube clone

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState,useEffect} from 'react'
+import { Box,Typography } from '@mui/material'
+import Videos from './Videos'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+import { useParams } from 'react-router-dom'
+
+
+const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  console.log(videos);
+  const { searchTerm }=useParams();
+  useEffect(()=>{
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then
+    ((data)=>setVideos(data.items));
+  },[searchTerm]);
+  console.log(videos);
+  return (
+    <Box p={2} sx={{overflowX:"auto",height:"90vh",flex:2}}>
+        <Typography variant='h4' fontWeight={"bold"} mb={2}  sx={{color:"white",}} >
+          Search result for <span style={{color:"#F31503",marginLeft:"5px",}}>{searchTerm}</span>Videos
+        </Typography>
+        <Videos videos={videos}/>
+      </Box>
+  )
+}
+
+export default SearchFeed;
---
+++ src/components/index.jsx
@@ -0,0 +1,13 @@
+export {default as Navbar} from "../components/Navbar"
+export {default as Feed} from "../components/Feed"
+export {default as ChannelDetail} from "../components/ChannelDetail"
+export {default as SearchFeed} from "../components/SearchFeed"
+export {default as VideoDetail} from "../components/VideoDetail"
+export {default as SearchBar} from "../components/SearchBar"
+export {default as Videos} from "../components/Videos"
+export {default as VideoCard} from "../components/VideoCard"
+export {default as ChannelCard} from "../components/ChannelCard"
+
+
+
+
